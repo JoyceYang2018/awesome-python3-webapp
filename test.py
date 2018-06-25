d23d0eef9b1918c4b98de097a067c378e21c1e90
JoyceYang2018 <384381523@qq.com>
2018-06-24 19:39:00 -0700

    create

---
+++ test.py
@@ -0,0 +1,19 @@
+from models import User,Blog,Comment
+
+import orm,asyncio
+
+
+
+async def test():
+    await orm.create_pool(loop=loop,user='www-data',password='www-data',db='awesome')
+
+    u=User(name='Test',email='test@example.com',passwd='1234567890',image='about:blank')
+
+    await u.save()
+    await orm.destroy_pool()
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
+loop.close()
